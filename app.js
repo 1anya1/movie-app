9c02094ef1dd39d687cfe375655130fe70b8e31b
Anna <anya@Annas-MacBook-Pro.local>
2020-01-30 18:46:12 -0800

    handle change / submit'

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 class App extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state={
+            baseURL:'http://www.omdbapi.com/?',
+            apikey: 'apikey=' + '6106d4d3',
+            query: '&t=',
+            movieTitle: '',
+            searchURL: ''
+        }
+    }
+    handleChange = (event) =>{
+        this.setState({ [event.target.id]: event.target.value });
+    };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            searchURL: this.state.baseURL + this.state.apikey +this.state.query+this.state.movieTitle
+        })
+    }
     render() {
         return (
-            <div>Hello World</div>
+            <React.Fragment>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='movieTitle'>Title</label>
+                    <input
+                    id='movieTitle'
+                    type='text'
+                    value={this.state.movieTitle}
+                    onChange={this.handleChange} />
+                    <input
+                    type='submit'
+                    value='Find Movie' />
+                </form>
+                <a href={this.state.searchURL}>{this.state.searchURL}</a>
+            </React.Fragment>
+            
         )
     }
 }
 
-class OMDBQueryForm extends React.Component {
-constructor (props) {
-    super(props)
-    this.state={
-        baseURL:'http://www.omdbapi.com/?',
-        apikey: 'apikey=' + '6106d4d3',
-        query: '&t=',
-        movieTitle: '',
-        searchURL: ''
-    }
-}
-}
 
 ReactDOM.render(
     <App />,
